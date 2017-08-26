e0006873231b8790b6c76bbdb4a475d48220a09c
Ryan Noelk <ryannoelk@gmail.com>
2017-08-25 21:18:42 -0400

    adding more to the models

--- api/v1/recipe_groups/schema.py
+++ api/v1/recipe_groups/schema.py
@@ -41,25 +41,56 @@ class RecipeGroupQuery(graphene.AbstractType):
     all_tags = DjangoFilterConnectionField(TagNode)
 
 
-class DeleteCuisine(DeleteModel, DeleteMutation):
-    class Config:
-        model = Cuisine
+class GroceryCuisineInput(graphene.InputObjectType):
+    id = graphene.ID()
+    title = graphene.String()
 
 
 class CreateCuisine(graphene.Mutation):
     class Input:
-        title = graphene.String()
+        data = graphene.Argument(GroceryCuisineInput)
 
     cuisine = graphene.Field(lambda: CuisineNode)
 
     @staticmethod
     def mutate(root, args, context, info):
-        title = args.get('title')
+        title = args.get('data').get('title')
         cuisine, created = Cuisine.objects.get_or_create(title=title)
         cuisine.save()
         return CreateCuisine(cuisine=cuisine)
 
 
+class DeleteCuisine(DeleteModel, DeleteMutation):
+    class Config:
+        model = Cuisine
+
+
+class GroceryCourseInput(graphene.InputObjectType):
+    id = graphene.ID()
+    title = graphene.String()
+
+
+class CreateCourse(graphene.Mutation):
+    class Input:
+        data = graphene.Argument(GroceryCuisineInput)
+
+    course = graphene.Field(lambda: CourseNode)
+
+    @staticmethod
+    def mutate(root, args, context, info):
+        title = args.get('data').get('title')
+        course, created = Course.objects.get_or_create(title=title)
+        course.save()
+        return CreateCourse(course=course)
+
+
+class DeleteCourse(DeleteModel, DeleteMutation):
+    class Config:
+        model = Course
+
+
 class RecipeGroupMutations(graphene.AbstractType):
     create_cuisine = CreateCuisine.Field()
     delete_cuisine = DeleteCuisine.Field()
+    create_course = CreateCourse.Field()
+    delete_course = DeleteCourse.Field()
